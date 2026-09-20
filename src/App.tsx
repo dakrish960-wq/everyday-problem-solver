6197e42e3de3ee21d12ee2c1fabc141660300c13
dakrish960-wq <dakrish960@gmail.com>
2026-09-20 10:21:51 +0530

    Import DiscountTool component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import { solutions, type Solution } from "./data/solutions";
+import DiscountTool from "./tools/DiscountTool";
 import {
   Search,
   Home,
